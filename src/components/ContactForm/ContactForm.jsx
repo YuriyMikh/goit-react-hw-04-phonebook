55f4802eba8164445b8cd41ce6e8e7cfa9d9f8f4
YuriyMikh <pamukkale1717@gmail.com>
2023-07-02 02:33:16 +0300

    refactoring ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,5 @@
 // import PropTypes from 'prop-types';
-import { Component } from 'react';
+import { useState } from 'react';
 import {
   StyledButton,
   StyledForm,
@@ -7,60 +7,123 @@ import {
   StyledLabel,
 } from './ContactForm.styled';
 
-const INITIAL_STATE = {
-  name: '',
-  number: '',
+export const ContactForm = () => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleChange = event => {
+    const { name, value } = event.target; //деструктуризация по наименованию свойства name тэга input
+
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+
+      case 'number':
+        setName(value);
+        break;
+
+      default:
+        return;
+    }
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    // this.props.onSubmit(this.state); //в файл App.jsx передаем пропсом данные после сабмита
+    reset();
+  };
+
+  const reset = () => {
+    setName('');
+    setNumber('');
+  };
+
+  return (
+    <StyledForm onSubmit={handleSubmit}>
+      <StyledLabel>
+        Name
+        <StyledInput
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleChange}
+          pattern="^[a-zA-Zа-яА-Я]+((?:'[a-zA-Zа-яА-Я\s])?(?:-[a-zA-Zа-яА-Я])?[a-zA-Zа-яА-Я\s]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </StyledLabel>
+
+      <StyledLabel>
+        Number
+        <StyledInput
+          type="tel"
+          name="number"
+          value={number}
+          onChange={handleChange}
+          pattern="\+?\d{1,4}[\s]?[\-]?\(?\d{1,3}?\)?[\s]?[\-]?\d{1,4}[\s]?[\-]?\d{1,4}[\s]?[\-]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+      </StyledLabel>
+      <StyledButton type="submit">Add contact</StyledButton>
+    </StyledForm>
+  );
 };
 
-export class ContactForm extends Component {
-  state = { ...INITIAL_STATE };
+// const INITIAL_STATE = {
+//   name: '',
+//   number: '',
+// };
 
-  handleChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
+// export class ContactForm extends Component {
+//   state = { ...INITIAL_STATE };
 
-  handleSubmit = event => {
-    event.preventDefault();
-    this.props.onSubmit(this.state); //в файл App.jsx передаем пропсом данные после сабмита
-    this.reset();
-  };
+//   handleChange = event => {
+//     this.setState({ [event.target.name]: event.target.value });
+//   };
 
-  reset = () => {
-    this.setState({ ...INITIAL_STATE });
-  };
+//   handleSubmit = event => {
+//     event.preventDefault();
+//     this.props.onSubmit(this.state); //в файл App.jsx передаем пропсом данные после сабмита
+//     this.reset();
+//   };
 
-  render() {
-    const { name, number } = this.state;
-    return (
-      <StyledForm onSubmit={this.handleSubmit}>
-        <StyledLabel>
-          Name
-          <StyledInput
-            type="text"
-            name="name"
-            value={name}
-            onChange={this.handleChange}
-            pattern="^[a-zA-Zа-яА-Я]+((?:'[a-zA-Zа-яА-Я\s])?(?:-[a-zA-Zа-яА-Я])?[a-zA-Zа-яА-Я\s]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-        </StyledLabel>
+//   reset = () => {
+//     this.setState({ ...INITIAL_STATE });
+//   };
 
-        <StyledLabel>
-          Number
-          <StyledInput
-            type="tel"
-            name="number"
-            value={number}
-            onChange={this.handleChange}
-            pattern="\+?\d{1,4}[\s]?[\-]?\(?\d{1,3}?\)?[\s]?[\-]?\d{1,4}[\s]?[\-]?\d{1,4}[\s]?[\-]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-          />
-        </StyledLabel>
-        <StyledButton type="submit">Add contact</StyledButton>
-      </StyledForm>
-    );
-  }
-}
+//   render() {
+//     const { name, number } = this.state;
+//     return (
+// <StyledForm onSubmit={this.handleSubmit}>
+//   <StyledLabel>
+//     Name
+//     <StyledInput
+//       type="text"
+//       name="name"
+//       value={name}
+//       onChange={this.handleChange}
+//       pattern="^[a-zA-Zа-яА-Я]+((?:'[a-zA-Zа-яА-Я\s])?(?:-[a-zA-Zа-яА-Я])?[a-zA-Zа-яА-Я\s]*)*$"
+//       title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+//       required
+//     />
+//   </StyledLabel>
 
+//   <StyledLabel>
+//     Number
+//     <StyledInput
+//       type="tel"
+//       name="number"
+//       value={number}
+//       onChange={this.handleChange}
+//       pattern="\+?\d{1,4}[\s]?[\-]?\(?\d{1,3}?\)?[\s]?[\-]?\d{1,4}[\s]?[\-]?\d{1,4}[\s]?[\-]?\d{1,9}"
+//       title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+//       required
+//     />
+//   </StyledLabel>
+//   <StyledButton type="submit">Add contact</StyledButton>
+// </StyledForm>
+//     );
+//   }
+// }
