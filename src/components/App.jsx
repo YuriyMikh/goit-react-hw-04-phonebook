5afd0f105376eb6fb623e173fc8948938659145e
YuriyMikh <pamukkale1717@gmail.com>
2023-07-03 01:39:39 +0300

    refactoring (changed to hooks)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { ContactForm } from 'components/ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
@@ -7,81 +7,64 @@ import { save, load } from '../utils/local-storage';
 
 const LOCALSTORAGE_KEY = 'contacts';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() {
-    const data = load(LOCALSTORAGE_KEY) ?? []; //?? - оператор нулевого слияния
-    this.setState({ contacts: data });
-  }
+  //при первой загрузке проверяем localStorage, если что-то уже есть - записываем в contacts
+  useEffect(() => {
+    const data = load(LOCALSTORAGE_KEY) ?? [];
+    setContacts(data);
+  }, []);
 
-  componentDidUpdate(prevProps, prevState) {
-    const { contacts } = this.state;
-    if (contacts !== prevState.contacts) {
-      save(LOCALSTORAGE_KEY, contacts);
-    }
-  }
+  //при перезаписи contacts записываем это в localStorage
+  useEffect(() => {
+    save(LOCALSTORAGE_KEY, contacts);
+  }, [contacts]);
 
-  onSubmit = ({ name, number }) => {
+  //функция принимает два параметра из компонента ContactForm
+  const onSubmit = (name, number) => {
+    //создаёт новый контакт
     const newContact = {
       id: crypto.randomUUID(),
-      name,
-      number,
+      name, //короткая форма записи
+      number, //короткая форма записи
     };
 
-    if (this.state.contacts.find(contact => contact.name === name)) {
+    //если contacts уже содержит контакт с таким именем - выводим alert
+    if (contacts.find(contact => contact.name === name)) {
       alert(`${name} is already in contacts`);
       return;
     }
-
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, newContact],
-    }));
+    //если не содержит - записываем в contacts
+    setContacts(prev => [...prev, newContact]);
   };
 
-  handleChangeFilter = event => {
-    this.setState({ filter: event.target.value });
+  //записываем то, что пользователь вводит в инпут поиска
+  const handleChangeFilter = event => {
+    setFilter(event.target.value);
   };
 
-  getFilteredData = () => {
-    const { contacts, filter } = this.state;
+  //в компонент ContactList передаём пользователей для отрисовки (после фильтрации)
+  const getFilteredData = () => {
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter.toLowerCase().trim())
     );
   };
 
-  handleDelete = id => {
-    this.setState({
-      contacts: this.state.contacts.filter(contact => contact.id !== id),
-    });
+  //удаляем контакт по id
+  const handleDelete = id => {
+    setContacts(prev => prev.filter(contact => contact.id !== id));
   };
 
-  render() {
-    return (
-      <AppContainer>
-        <h1>Phonebook</h1>
-        <ContactForm onSubmit={this.onSubmit} />
+  return (
+    <AppContainer>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={onSubmit} />
 
-        <h2>Contacts</h2>
-        <Filter
-          filterValue={this.state.filter}
-          filterChange={this.handleChangeFilter}
-        />
-        <ContactList
-          contacts={this.getFilteredData()}
-          deleteContact={this.handleDelete}
-        />
-      </AppContainer>
-    );
-  }
-}
-
-export default App;
+      <h2>Contacts</h2>
+      <Filter filterValue={filter} filterChange={handleChangeFilter} />
+      <ContactList contacts={getFilteredData()} deleteContact={handleDelete} />
+    </AppContainer>
+  );
+};
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,4 @@
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import { useState } from 'react';
 import {
   StyledButton,
@@ -7,20 +7,20 @@ import {
   StyledLabel,
 } from './ContactForm.styled';
 
-export const ContactForm = () => {
+export const ContactForm = ({ onSubmit }) => {
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
 
   const handleChange = event => {
     const { name, value } = event.target; //деструктуризация по наименованию свойства name тэга input
-
+    //через switch записываем результаты в setName и setNumber
     switch (name) {
       case 'name':
         setName(value);
         break;
 
       case 'number':
-        setName(value);
+        setNumber(value);
         break;
 
       default:
@@ -30,8 +30,8 @@ export const ContactForm = () => {
 
   const handleSubmit = event => {
     event.preventDefault();
-    // this.props.onSubmit(this.state); //в файл App.jsx передаем пропсом данные после сабмита
-    reset();
+    onSubmit(name, number); //в файл App.jsx передаем пропсом данные после сабмита
+    reset(); //очищаем инпуты
   };
 
   const reset = () => {
@@ -71,59 +71,6 @@ export const ContactForm = () => {
   );
 };
 
-// const INITIAL_STATE = {
-//   name: '',
-//   number: '',
-// };
-
-// export class ContactForm extends Component {
-//   state = { ...INITIAL_STATE };
-
-//   handleChange = event => {
-//     this.setState({ [event.target.name]: event.target.value });
-//   };
-
-//   handleSubmit = event => {
-//     event.preventDefault();
-//     this.props.onSubmit(this.state); //в файл App.jsx передаем пропсом данные после сабмита
-//     this.reset();
-//   };
-
-//   reset = () => {
-//     this.setState({ ...INITIAL_STATE });
-//   };
-
-//   render() {
-//     const { name, number } = this.state;
-//     return (
-// <StyledForm onSubmit={this.handleSubmit}>
-//   <StyledLabel>
-//     Name
-//     <StyledInput
-//       type="text"
-//       name="name"
-//       value={name}
-//       onChange={this.handleChange}
-//       pattern="^[a-zA-Zа-яА-Я]+((?:'[a-zA-Zа-яА-Я\s])?(?:-[a-zA-Zа-яА-Я])?[a-zA-Zа-яА-Я\s]*)*$"
-//       title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-//       required
-//     />
-//   </StyledLabel>
-
-//   <StyledLabel>
-//     Number
-//     <StyledInput
-//       type="tel"
-//       name="number"
-//       value={number}
-//       onChange={this.handleChange}
-//       pattern="\+?\d{1,4}[\s]?[\-]?\(?\d{1,3}?\)?[\s]?[\-]?\d{1,4}[\s]?[\-]?\d{1,4}[\s]?[\-]?\d{1,9}"
-//       title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-//       required
-//     />
-//   </StyledLabel>
-//   <StyledButton type="submit">Add contact</StyledButton>
-// </StyledForm>
-//     );
-//   }
-// }
+ContactForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
